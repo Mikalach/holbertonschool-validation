15244231965328937dbd6e4c37c29d155495645f
Mikalach <mika.djetic@gmaiul.com>
2023-05-08 09:15:38 -0700

    indentation problems should be solved

--- module2_task2/main.go
+++ module2_task2/main.go
@@ -40,38 +40,36 @@ func HealthCheckHandler(w http.ResponseWriter, r *http.Request) {
 
   // Write the string "ALIVE" into the response's body
   io.WriteString(w, "ALIVE")
-
+  
   // End of the function: return HTTP 200 by default
+}
+  
+func HelloHandler(w http.ResponseWriter, r *http.Request) {
+  // Extract the query parameters from the GET request
+  queryParams := r.URL.Query()
 
-  func HelloHandler(w http.ResponseWriter, r *http.Request) {
-    // Extract the query parameters from the GET request
-    queryParams := r.URL.Query()
+// Retrieve the query parameters with the key "name"
+  nameParams := queryParams["name"]
   
-    // Retrieve the query parameters with the key "name"
-    nameParams := queryParams["name"]
-  
-    var name string
-    switch len(nameParams) {
-       case 0:
-         // Set the name variable to there when there is no parameter "name" in the request
-         name = "there"
-       default:
-         // Set the name variable to the first parameter "name" in the request
-         name = nameParams[0]
-    }
-  
-    // Return status 400 if name is empty
-    if name == "" {
-      w.WriteHeader(400)
-      return
-    }
-  
-    // Write the string "Hello <name>" into the response's body
-    _, _ = io.WriteString(w, fmt.Sprintf("Hello %s!", name))
-  
-    // Print a line in the ACCESS log
-    fmt.Printf("HIT: hello handler with name %s \n", name)
+  var name string
+  switch len(nameParams) {
+    case 0:
+       // Set the name variable to there when there is no parameter "name" in the request
+      name = "there"
+      default:
+       // Set the name variable to the first parameter "name" in the request
+       name = nameParams[0]
   }
 
-
+  // Return status 400 if name is empty
+  if name == "" {
+    w.WriteHeader(400)
+    return
+  }
+  
+  // Write the string "Hello <name>" into the response's body
+  _, _ = io.WriteString(w, fmt.Sprintf("Hello %s!", name))
+  
+  // Print a line in the ACCESS log
+  fmt.Printf("HIT: hello handler with name %s \n", name)
 }
